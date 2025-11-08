e4ecbb3a347189944bd64c5b93f5a0fb49595f81
Maryam Moghadasi <moghadasi.64@gmail.com>
2025-11-08 16:13:35 +0100

    CSv files

---
+++ data_extract_load/load_data_jobs.py
@@ -0,0 +1,76 @@
+import dlt
+import requests
+import json
+
+dlt.config["load.truncate_staging_dataset"] = True
+
+query = ""
+table_name = "job_ads"
+occupation_fields = ("j7Cq_ZJe_GkT", "9puE_nYg_crq", "MVqp_eS8_kDZ")
+params = {"q": query, "limit": 100, "occupation-field": occupation_fields}
+
+def _get_ads(url_for_search, params):
+    headers = {"accept": "application/json"}
+    response = requests.get(url_for_search, headers=headers, params=params)
+    response.raise_for_status()  # check for http errors
+    return json.loads(response.content.decode("utf8"))
+
+
+@dlt.resource(table_name= "job_ads",write_disposition="append")
+def jobsearch_resource(params):
+    url = "https://jobsearch.api.jobtechdev.se"
+    url_for_search = f"{url}/search"
+    limit = params.get("limit", 100)
+    offset = 0
+
+    while True:
+        # build this page’s params
+        page_params = dict(params, offset=offset)
+        data = _get_ads(url_for_search, page_params)
+
+        hits = data.get("hits", [])
+        if not hits:
+            # no more results
+            break
+
+        # yield each ad on this page
+        for ad in hits:
+            yield ad
+
+        # if fewer than a full page was returned, we’re done
+        if len(hits) < limit or offset > 1900:
+            break
+
+        offset += limit
+
+@dlt.source
+def jobsearch_source():
+    
+    return jobsearch_resource(params)
+
+
+#def run_pipeline(query, table_name, occupation_fields):
+    # pipeline = dlt.pipeline(
+    #     pipeline_name="HRpipeline",
+    #     destination="snowflake",
+    #     dataset_name="staging",
+ #   )
+
+    # for occupation_field in occupation_fields:
+    #     params = {"q": query, "limit": 100, "occupation-field": occupation_field}
+    #     load_info = pipeline.run(
+    #         jobsearch_resource(params=params), table_name=table_name
+    #     )
+    #     print(f"Occupation field: {occupation_field}")
+    #     print(load_info)
+
+
+
+
+
+    # Bygg och anläggning, "Kultur, media, design", "Pedagogik"
+    # run_pipeline("", "job_ads", ("j7Cq_ZJe_GkT", "9puE_nYg_crq", "MVqp_eS8_kDZ"))
+    
+ 
+
+   
